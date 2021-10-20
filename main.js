bbfa98a2a3e7657ff9502d2e5b9731a2da19e974
SleepyC4 <nikolajcarlslund@gmail.com>
2021-10-20 08:54:02 +0200

    changed

--- main.js
+++ main.js
@@ -1,17 +1,1 @@
-//const product = 'Socks'
-
-const app = Vue.createApp({
-    // data: function(){
-    //     return{
-    //         product: 'Socks'
-    //     }
-    // }
-    data(){
-        return{
-            product: {
-                name: 'Socks',
-                description: 'These socks are very nice!'
-            }
-        }
-    }
-});
+const product = 'Socks'
