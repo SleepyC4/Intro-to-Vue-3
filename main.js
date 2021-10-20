a8d697ab7167dae10d42dc35d13e6a2172ade51b
SleepyC4 <nikolajcarlslund@gmail.com>
2021-10-20 08:55:20 +0200

    feat(vue): setup vue

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-const product = 'Socks'
+//const product = 'Socks'
+
+const app = Vue.createApp({
+    // data: function(){
+    //     return{
+    //         product: 'Socks'
+    //     }
+    // }
+    data(){
+        return{
+            product: {
+                name: 'Socks',
+                description: 'These socks are very nice!'
+            }
+        }
+    }
+});
