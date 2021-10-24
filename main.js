ed816d9e994717b21d535740f1c1c07c080fe542
SleepyC4 <nikolajcarlslund@gmail.com>
2021-10-24 17:26:01 +0200

    finished lesson 3

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.google.dk/search?q=picture&sxsrf=AOaemvJ8fJPw_lcE22o5cW6boJ96aN5uEA:1634713322398&tbm=isch&source=iu&ictx=1&fir=YXgcNflll9kS8M%252C0N6qTt3P-PlepM%252C_%253B5DWXdgFIbPsf8M%252CMcaww4JtA8IBqM%252C_%253BHc5jHkS6Vw5sZM%252CaPImLvUOjpk0SM%252C_%253BCpAzRfj8BL3ElM%252C6bQW6ChCsNFmzM%252C_%253BYhC3SDO6KWPOgM%252CxXPMDGfQTdYieM%252C_%253Bw2DfUiGh9nbK5M%252CgnBbb_yW79vLUM%252C_%253BRCuirfZGoxyy3M%252CvCSxXmJn2CS2OM%252C_%253Bh5i2EOL5UAOCcM%252C_BEPD4FRGQhqRM%252C_%253B08ndcIvlSDHMbM%252C2Vscex7JgttYMM%252C_%253Bc7nrrrPa7_fFOM%252C1GgYJi2MMArQyM%252C_%253BJENRDkRIxN702M%252CHAYMQ7wDoUAMBM%252C_%253BxEqGyMTovdo6sM%252CPiIFi2O8fwUmEM%252C_%253BJcYI-OFcfvGzkM%252CaYIZSEDwq-ua0M%252C_&vet=1&usg=AI4_-kTgqATuzZ4sPaLvNtAjWVlV6VdRQg&sa=X&ved=2ahUKEwie94_XtdjzAhVHhv0HHQTOCrcQ9QF6BAgaEAE&biw=1920&bih=969&dpr=1#imgrc=Hc5jHkS6Vw5sZM'
         }
     }
 })
